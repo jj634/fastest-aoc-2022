a44422e5ca9ebef5ed792c0d85555da091e82583
Jason Jung <jj634@cornell.edu>
2022-12-04 19:45:33 -0800

    use timeit to get consistent results. also remove mem

--- 1/helpers.py
+++ 1/helpers.py
@@ -1,5 +1,6 @@
-import time
+import timeit
 import os, psutil
+from memory_profiler import memory_usage
 
 def get_data() -> str:
 	""" Returns the problem input as a string. """
@@ -9,13 +10,9 @@ def get_data() -> str:
 
 def benchmark(solution_fn):
 	""" Prints the runtime and memory usage of solution_fn(). """
-	start_time = time.time()
+	num_repetitions=5000
+	elapsed_time_ms = round(timeit.timeit(stmt=solution_fn,number=num_repetitions)/num_repetitions * 1000,4)
+	
+	# memory_used_mib=max(memory_usage((solution_fn, (), {}),interval=1e-6, timeout=0.01))
 
-	solution_fn()
-
-	elapsed_time_ms = round((time.time() - start_time)*1000,4)
-
-	process = psutil.Process(os.getpid())
-	memory_used_mb=process.memory_info().rss / 2**20
-
-	print("%s ms, %s MB" % (elapsed_time_ms, memory_used_mb))
+	print("%s ms" % (elapsed_time_ms))
